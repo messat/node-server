c5a00c36a968d02a8d712d941bd637c2bda78360
messat <148489126+messat@users.noreply.github.com>
2024-09-16 22:06:07 +0100

    Added /api/authors endpoint reading authors.json file

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
-var http = require('http');
+var http = require("http");
 
-const fs = require('fs/promises')
+const fs = require("fs/promises");
 
 const server = http.createServer(async (request, response) => {
-    const {method, url} = request
-    if (url === '/api' && method === "GET"){
-        response.statusCode = 200
-        response.write(JSON.stringify({message: "Hello!"}))
-        response.end()
-    } else if (url === '/api/books' && method === "GET"){
-        try {
-            const books = await fs.readFile('./data/books.json')
-            const booksData = JSON.parse(books.toString())
-            response.write(JSON.stringify(booksData))
-            response.statusCode = 200
-            response.end()
-        } catch (e){
-            console.log(e)
-        }
+  const { method, url } = request;
+  if (url === "/api" && method === "GET") {
+    response.statusCode = 200;
+    response.write(JSON.stringify({ message: "Hello!" }));
+    response.end();
+  } else if (url === "/api/books" && method === "GET") {
+    try {
+      const books = await fs.readFile("./data/books.json");
+      const booksData = JSON.parse(books.toString());
+      response.write(JSON.stringify(booksData));
+      response.statusCode = 200;
+      response.end();
+    } catch (e) {
+      console.log(e);
     }
-  });
-  
+  } else if(method === "GET" && url === "/api/authors"){
+    try {
+      const authors = await fs.readFile("./data/authors.json")
+      const authorsData = JSON.parse(authors.toString())
+      response.write(JSON.stringify(authorsData)) 
+      response.end()  
+    } catch (e){
+        console.log(e)
+    }
+  }
+});
 
-server.listen(8080, (err)=>{
-    if (err) console.log(err)
-    else console.log("logged using port 8080")
-})
+server.listen(8080, (err) => {
+  if (err) console.log(err);
+  else console.log("logged using port 8080");
+});
