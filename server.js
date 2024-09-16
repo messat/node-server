2fff8ad70ddf2d7ea3202e96610b78226ef1cc1a
messat <148489126+messat@users.noreply.github.com>
2024-09-16 21:51:23 +0100

    New endpoint made to read and write  books.json file

---
+++ server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+const fs = require('fs/promises')
+
+const server = http.createServer(async (request, response) => {
+    const {method, url} = request
+    if (url === '/api' && method === "GET"){
+        response.statusCode = 200
+        response.write(JSON.stringify({message: "Hello!"}))
+        response.end()
+    } else if (url === '/api/books' && method === "GET"){
+        response.statusCode = 200
+        try {
+            const books = await fs.readFile('./data/books.json')
+            const booksData = JSON.parse(books.toString())
+            response.write(JSON.stringify(booksData))
+            response.end()
+        } catch (e){
+            console.log(e)
+        }
+    }
+  });
+  
+
+server.listen(8080, (err)=>{
+    if (err) console.log(err)
+    else console.log("logged using port 8080")
+})
